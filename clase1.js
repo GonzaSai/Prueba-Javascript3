c845e16ddb1e86a2dd07142d66196ad310bb3fc5
alumno <alumno@Instituto.local>
2023-08-28 10:52:43 -0300

    Prueba Javascript 3
    No se :)

---
+++ clase1.js
@@ -0,0 +1,17 @@
+//1
+
+function cuadrado(x)
+{
+    let numero = x * x
+    return 'El resultado de 8 elevado a cuadrado es ' + numero
+}
+console.log(cuadrado(8))
+
+//2
+
+function cubo(x)
+{
+    let numero = x * x * x
+    return 'El resultado de 5 elevado a cubo es ' + numero
+}
+console.log(cuadrado(5))
